b10fbc2c78d6943bf6d8ea435c35ce7866a5ea75
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-04-25 15:59:45 +0300

    lab01b1

---
+++ JS/lab01b.js
@@ -0,0 +1,19 @@
+"usr strict"
+let name = prompt('what is your name')
+alert('Hello '+ name);
+let num1 = parseInt( prompt('please, Enter first number'))
+let num2 = parseInt( prompt('please, Enter second number'))
+let op = prompt('please, Enter the oparation : summation or multiplication')
+let sum = num1+num2
+let multip = num1 * num2
+if(op=='summation')
+alert('the summation is ' + sum)
+else if (op== 'multiplication')
+alert('the multiplication is '+multip)
+
+console.log('Hello '+ name)
+console.log('First number= '+num1)
+console.log('second number= '+num2)
+console.log('summation= '+ sum)
+console.log('multiplication= '+ multip)
+
